347dc436f2765578f72d80a60b11fc2ecf83beb3
amiramohiey <ameramohiey92@gmail.com>
2017-05-01 15:56:32 +0200

    adding project's template files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Cafeteria
 Rails project for running cafeteria ordering system managemnet
+<h1>Installation</h1>
+rails v 5.0.2 
+clone the repo 
+run bundle install 
+create mysql database called cafeteria_production 
+create mysql user="cafeteria" and password ="12345"
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+      class UsersController < ApplicationController
+      before_action :user_data , only: [:show,:update,:destroy,:edit]
+  def index
+     @users = User.all
+     @current_user = User.find_by_id(session[:current_user_id])
+    
+  end
+  def show
+    
+  end
+  def new
+      @user=User.new
+  end
+  def create
+      @user = User.new(params[:user].permit(:name, :email, :password))
+ 
+     if @user.save
+      redirect_to @user
+     else render "new"
+     end
+
+  end
+  def edit
+  end
+  def update
+      if @user.update(user_params)
+            redirect_to @user
+      else render "edit"
+      end
+  end
+  def destroy
+       
+       @user.destroy
+       redirect_to users_path
+  end
+  def user_params
+      
+    params.require(:user).permit(:name, :email, :password)
+  end
+ def user_data
+    #  @user = User.find(params[:id])
+      
+    
+ end
+ end

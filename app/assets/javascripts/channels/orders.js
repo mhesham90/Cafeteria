a55ae54672baff66e3adc7d3630d3e2f122988c9
mhesham90 <mohammedhesham.1990@gmail.com>
2017-05-06 13:12:16 +0200

    handeled create and cancel order actioncable js

--- app/assets/javascripts/channels/orders.js
+++ app/assets/javascripts/channels/orders.js
@@ -9,37 +9,47 @@ App.orders = App.cable.subscriptions.create("OrdersChannel", {
 
   received: function(data) {
       var order = JSON.parse(data.order);
-      if (data.type === 'cancel') {
-          var html = '<form class="edit_order" id="edit_order_'+order.id+'" action="/orders/'+order.id+'" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" ></form>' +
-              '<input type="hidden" name="_method" value="patch" /><input type="hidden" name="authenticity_token" value="'+$('meta[name=csrf-token]').attr('content')+'" />'+
-              '<div></div>'+'<tr data-toggle="collapse" data-target="#'+order.id+'" class="clickable">'+
+      if (data.type === 'create') {
+          var html = '<tr data-toggle="collapse" data-target="#'+order.id+'" class="clickable">'+
               '<td>'+order.created_at+'</td>'+
               '<td>'+order.user.first_name+'</td>'+
               '<td>'+order.user.room.name+'</td>'+
               '<td>'+order.total +'</td>'+
               '<td>' +
-              '<input type="submit" name="commit" value="process" class="btn btn-default" data-disable-with="process" />' +
+              '<form class="edit_order" id="edit_order_'+order.id+'" action="/orders/'+order.id+'" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" >' +
+              '<input type="hidden" name="_method" value="patch" /><input type="hidden" name="authenticity_token" value="'+$('meta[name=csrf-token]').attr('content')+'" />'+
+              '<input type="submit" name="commit" value="process" class="btn btn-info" data-disable-with="process" />' +
               '<input class="hidden" style="width:100%" value="1" type="number" name="order[status]" id="order_status" />' +
-              '</td>'+
+              '</form></td>'+
               '</tr>'+
               '<tr><td colspan="5">'+
               '<div id="'+order.id+'" class="collapse">';
+
           order.orderdetails.forEach(function(ord){
                   html+= '<div class="col-md-4">' +
-                      '<div class="panel panel-default">'+
-                      '<div class="panel-heading">'+ord.product.name+'</div>' +
-                      '<div class="panel-body"> ' +
-                      '<img src="https://res.cloudinary.com/cafeteria/image/upload/v1/products/'+ord.product.id+'/thumb/'+ord.product.avatar_file_name+'", class: "img-thumbnail">' +
-                      '</div>' +
-                      '<div class="panel-footer">'+ord.product.price+' LE</div>' +
-                      '</div>' +
-                      '</div>'
+                      '<div class="card">'+
+                      '<img src="https://res.cloudinary.com/cafeteria/image/upload/v1/products/'+ord.product.id+'/original/'+ord.product.avatar_file_name+'">' +
+                      '<div class="card-block">'+
+                      '<h3 class="card-title">'+ord.product.name+'</h3><hr>' +
+                      '<p class="card-text"><B>price : </B><label>'+ord.product.price+'</label> LE' +
+                      '<B style=" padding-left:5%">Quantity : </B><label>'+ ord.quantity+'</label>' +
+                      '</div></div></div>'
               });
-              
-              html+= '</div></td></tr>';
-              $('tbody').prepend(html);
+
+          html+= '</div></td></tr>';
+          $('tbody').prepend(html);
       }
-    console.log(data)
+      else if (data.type === 'cancel') {
+          var btn = '<button class="btn btn-danger cancelOrd">order canceled X</button>';
+          $('form#edit_order_'+order.id).replaceWith(btn);
+          $('.cancelOrd').on('click',function () {
+              $(this).closest('tr').next().remove();
+              $(this).closest('tr').remove();
+          })
+
+      }
+
+          console.log(data)
   //    $('meta[name=csrf-token]').attr('content')
   }
 });

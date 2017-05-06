23e0f9935d225457a3cc3333c8551a5b1c6e48c1
mhesham90 <mohammedhesham.1990@gmail.com>
2017-05-06 15:15:42 +0200

    fixed make order response

--- app/jobs/order_create_job.rb
+++ app/jobs/order_create_job.rb
@@ -2,6 +2,7 @@ class OrderCreateJob < ApplicationJob
   queue_as :default
 
   def perform(order)
+    data = {}
     data['type'] = 'create'
     data['order'] = order
     ActionCable.server.broadcast "orders:admin", data
